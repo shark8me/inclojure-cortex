5e33964d5333d30118c27def694c8fe6fe4b6b21
Kiran Karkera <kiran.karkera@gmail.com>
2017-12-23 12:31:44 +0800

    initial commit

---
+++ cortex-examples/project.clj
@@ -0,0 +1,18 @@
+(defproject cortex-examples "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins  [[lein-gorilla "0.4.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [thinktopic/experiment "0.9.23-SNAPSHOT"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 ;;If you need cuda 8...
+                 [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
+                 [org.shark8me/tfevent-sink "0.1.4"]
+                 [cc.artifice/clj-ml "0.8.5"]
+                 [com.rpl/specter "1.0.5"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [core-matrix-gorilla "0.1.0"]
+                 [net.mikera/core.matrix "0.61.0"]  
+                 ])
---
+++ docs/index.md
@@ -0,0 +1,20 @@
+---
+layout: default
+title: An introduction to Cortex
+---
+
+# An introduction to Cortex
+
+This is the source repository for a presentation on building Neural Networks in Clojure using the [Cortex](https://github.com/thinktopic/cortex) library.
+
+The presentation can be 
+* viewed on [Youtube](https://engineers.sg/video/neural-networks-in-clojure-singapore-clojure-meetup--1902)
+* slides are [available online](https://cdn.rawgit.com/shark8me/cortex-tensorboard-presentation/4ab391a3/docs/cortex.html) 
+* here's the [org-mode](https://cdn.rawgit.com/shark8me/cortex-tensorboard-presentation/4ab391a3/docs/cortex.org) source.
+
+## License
+
+Copyright © 2017 shark8me 
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
